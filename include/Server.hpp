1b005a2c8e5fa589e5d41180030c28cf39ce4838
anBarot <barot.anne@gmail.com>
2021-09-20 14:23:32 +0200

    add getters

--- include/Server.hpp
+++ include/Server.hpp
@@ -8,7 +8,7 @@ class Server
 {
 private:
 	unsigned							listen_port;
-	std::list<std::string>				name;
+	std::list<std::string>				names;
 	unsigned							max_body_size;
 	std::list<Location> 				locations;
 	std::map<unsigned, std::string>		error_page;
@@ -18,10 +18,14 @@ public:
 	~Server() {}
 
 	// getters
-	std::list<Location>	&get_location() { return this->locations; }
+	std::list<Location>					&get_location() 	{ return this->locations; }
+	unsigned 							get_listen_port()	{ return this->listen_port; }
+	std::list<std::string>				&get_name() 		{ return this->names; }
+	unsigned							get_max_body_size() { return this->max_body_size; }
+	std::map<unsigned, std::string>		get_error_page()	{ return this->error_page; }	
 
 	// setters
-	void	add_name(std::string n_name)							{ this->name.push_back(n_name); }
+	void	add_name(std::string n_name)							{ this->names.push_back(n_name); }
 	void	add_max_body(unsigned n_max_body)						{ this->max_body_size = n_max_body; }
 	void	add_listen_port(unsigned int n_port) 					{ this->listen_port = n_port; }
 	void	add_locations(Location n_locations)						{ this->locations.push_back(n_locations); }
