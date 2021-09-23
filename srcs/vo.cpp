e8a8f5d88c8678000d679dda2c9faa1d3208a973
Lubenica <ng.vosong@gmail.com>
2021-09-23 11:15:51 +0200

    add_clien()

--- srcs/vo.cpp
+++ srcs/vo.cpp
@@ -8,8 +8,7 @@ std::vector<Server> parsing_simulation()
 	return servers;
 }
 
-
-int 	read_socket(SocketPool *sp, std::vector<int> lsp, std::vector<Client> clients)
+void 	add_clients(SocketPool *sp, std::vector<int> lsp, std::vector<Client> clients)
 {
 	// iteration sur sp.listen_socket jusque trouver un is_set, et l'accepter
 	// créer un socket client à partir du socker => Client(socket)
@@ -20,14 +19,30 @@ int 	read_socket(SocketPool *sp, std::vector<int> lsp, std::vector<Client> clien
 		{
 			new_sd = accept(*it, 0, 0);
 			clients.push_back(Client::Client(new_sd));
-			break;
+			return;
+		}
+
+		if (FD_ISSET(*it, sp->writing_set))
+		{
+			new_sd = accept(*it, 0, 0);
+			clients.push_back(Client::Client(new_sd));
+			return;
 		}
 	}
-	// re iterer sur les socket clients voir si y'a un truc à lire
 }
 
 
-void 	write_socket(SocketPool *sp)
+int 	read_sockets(SocketPool *sp, std::vector<int> lsp, std::vector<Client> clients)
+{
+
+	for (std::vector<Client>::iterator it = clients.begin(); it != clients.end(); it++)
+	{
+
+	}
+}
+
+
+void 	write_sockets(SocketPool *sp)
 {
 	// itérer sur les clients voir si ils sont dispo en écriture
 }
@@ -39,6 +54,7 @@ int 	socket_routine(std::vector<int> listen_sockets_pool, std::vector<Client> cl
 
 	if (select(FD_SETSIZE, &sp.reading_set, &sp.writing_set, 0, 0) == -1)
 		error_and_exit(SOCK_ERR);
+	add_clients(&sp, listen_sockets_pool, clients_pool);
 	read_sockets(&sp, listen_sockets_pool, clients_pool);
 	write_sockets(&sp);
 }
