c5b2186e5100535253d25ec0cf1a64cc55a95995
anBarot <barot.anne@gmail.com>
2022-02-06 22:02:12 +0100

    changing index  page - site

--- www/cgi-bin/testpl.pl
+++ www/cgi-bin/testpl.pl
@@ -1,6 +1,5 @@
 #!/usr/bin/perl
 print "Content-type: text/html\n\n";
 print "<html><body>\n";
-print "<h1>Hello World</h1>\n";
-print "Using Perl<p>\n";
+<GIF src='../images/macron-perlimpinpin.gif'>;
 print "</body></html>\n";
