db491b91b820b9203bb7d9d180e0fe219d4ef48b
anBarot <barot.anne@gmail.com>
2022-02-07 00:06:54 +0100

    python test

--- www/cgi-bin/testpl.pl
+++ www/cgi-bin/testpl.pl
@@ -1,5 +1,15 @@
 #!/usr/bin/perl
+
 print "Content-type: text/html\n\n";
 print "<html><body>\n";
 <GIF src='../images/macron-perlimpinpin.gif'>;
 print "</body></html>\n";
+
+use Unix::Passwd::File;
+use File::Path;
+use CGI;
+use strict;
+
+my $cgi = new CGI;
+
+qx(perl index.pl);
