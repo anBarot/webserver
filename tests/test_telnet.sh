ced1b2de5d1e99314a5ad28b1deb42914630310a
Anne BAROT <abarot@e2r3p5.clusters.42paris.fr>
2022-02-08 22:47:08 +0100

    test shell

---
+++ tests/test_telnet.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+ 
+echo -e "**************  Test GET  *****************"
+
+{ 
+echo "GET / HTTP/1.1"; 
+echo "Host: localhost:3000"; 
+echo; sleep 1; 
+} | telnet localhost 3000
+
+echo -e "\n\n**************  Test DELETE  *****************"
+
+touch ./www/delete/to_del.txt
+{ 
+echo "DELETE /delete/to_del.txt HTTP/1.1"; 
+echo "Host: localhost:3000"; 
+echo; sleep 1; 
+} | telnet localhost 3000
+
+echo -e "\n\n**************  Test POST  *****************"
+
+#echo -e "\n\n**********  Test with content length  *************"
+#
+#{ 
+#echo "POST /file_to_upload.txt HTTP/1.1"; 
+#echo "Host: localhost:3000"; 
+#echo "content-length: 10";
+#echo "";
+#echo "123";
+#echo "456";
+#echo; sleep 1;
+#} | telnet localhost 3000
+
+echo -e "\n\n**********  Test with chunked  *************"
+
+{ 
+echo "POST /file_to_upload_2.txt HTTP/1.1"; 
+echo "Host: localhost:3000"; 
+echo "transfer-encoding: chunked";
+echo "";
+echo "5";
+echo "abc";
+echo "5";
+echo "def";
+echo "0";
+echo; sleep 1; 
+} | telnet localhost 3000
+
+echo -e "\n\n**********  Test with cgi  *************"
+
+#echo -e "\n\n**********  Python test  *************"
+#
+#{ 
+#echo "POST /testpy.py HTTP/1.1"; 
+#echo "Host: localhost:3000"; 
+#echo "content-length: 22";
+#echo ""
+#echo "lname=coco&fname=choco"
+#echo; sleep 1; 
+#} | telnet localhost 3000
+
+echo -e "\n\n**********  Perl test  *************"
+
+{ 
+echo "POST /testpl.pl HTTP/1.1"; 
+echo "Host: localhost:3000"; 
+echo "content-length: 0";
+echo; sleep 1; 
+} | telnet localhost 3000
+
+echo -e "\n\n**********  Bash test  *************"
+
+{ 
+echo "POST /testbash.sh HTTP/1.1"; 
+echo "Host: localhost:3000"; 
+echo "content-length: 0";
+echo; sleep 1; 
+} | telnet localhost 3000
